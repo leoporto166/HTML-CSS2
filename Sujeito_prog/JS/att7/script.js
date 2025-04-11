b63a6e95f52462d37283c139e756b366b26e1462
leoporto166 <leonardo.porto166@gmail.com>
2025-04-11 09:11:39 -0300

    JS

---
+++ Sujeito_prog/JS/att7/script.js
@@ -0,0 +1,46 @@
+
+
+//CONDICIONAIS E COMPARAÇÕES!
+
+var valor = 30;
+
+if(valor == 30){
+    console.log("Sim o valor é igual a 30!")
+}
+else{
+    console.log("O valor é diferente de 30!")
+}
+
+var string = "50"
+if(string === 50){
+    console.log("O valor é verdadeiramente 50!")
+}
+
+//COMPARAÇÕES COM STRING
+
+var nome = "Leoanardo"
+
+if(nome === "Leonardo"){
+    console.log("Bem vindo Leonardo!")
+}
+
+//COMPARAÇÕES COM VALORES BOOLEANOS
+
+var userOnline = true
+
+if(userOnline === true){
+    console.log("Usuario esta online!")
+}
+else{
+    console.log("Usuario está offline!")
+}
+
+// CONDICIONAIS TERNARIOS
+
+var numero = 30
+
+numero === 10 ? console.log("Número é igual a 10") : console.log ("Número diferente de 10")
+
+//? - IF
+//: - ELSE
+
