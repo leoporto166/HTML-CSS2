9f194524c32b04f98fcb84a41f6524c3d00f6fdc
leoporto166 <leonardo.porto166@gmail.com>
2025-04-17 17:10:38 -0300

    js

---
+++ Sujeito_prog/JS/att17/script2.js
@@ -0,0 +1,44 @@
+//Crie um array com 10 números e imprima apenas os números pares.
+
+let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+let pares = numeros.filter((item) => item % 2 === 0);
+
+console.log(pares)
+
+console.log("-----------------------------------------")
+
+//Adicione 3 nomes ao final de um array de nomes.
+
+let nomes = ["Leonardo", "Joao Pedro", "Kaua"];
+
+console.log(nomes)
+
+nomes.push("Kiwi", "Pig", "Leo")
+
+console.log(nomes)
+
+console.log("-----------------------------------------")
+
+//Remova o primeiro item de um array e imprima o array atualizado
+
+let itens = ["item1", "item2", "item3", "item4", "item5"]
+
+console.log(itens)
+
+itens.splice(0, 1)
+
+console.log(itens)
+
+console.log("-----------------------------------------")
+
+//Inverta a ordem de um array sem usar o .reverse(), só lógica.
+
+let sequencia = [1, 2, 3, 4, 5]
+let invertido = [];
+
+for(let i = sequencia.length - 1; i >=0; i--){
+    invertido.push(sequencia[i])
+}
+
+console.log(invertido)
